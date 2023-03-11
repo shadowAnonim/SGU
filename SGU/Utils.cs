76804744b73efffb1e43dda93906034b6e653e00
shadowAnonim <shadow.anonim@yandex.ru>
2023-03-11 16:18:59 +0300

    utils

---
+++ SGU/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SGU
+{
+    static internal class Utils
+    {
+        public static bool checkEmail(string input)
+        {
+            return Regex.IsMatch(input, "/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i");
+        }
+
+        public static bool checkPhone(string input)
+        {
+            return Regex.IsMatch(input, "/^\\+?(\\d{1,3})?[- .]?\\(?(?:\\d{2,3})\\)?[- .]?\\d\\d\\d[- .]?\\d\\d\\d\\d$/");
+        }
+    }
+}
