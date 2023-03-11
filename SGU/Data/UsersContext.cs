425eb12ec412fa1440276bce3d08e7c49e7a03da
shadowAnonim <shadow.anonim@yandex.ru>
2023-03-11 17:20:38 +0300

    DbContext

--- SGU/Data/UsersContext.cs
+++ SGU/Data/UsersContext.cs
@@ -34,8 +34,11 @@ public partial class UsersContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=C:\\Users\\Evgeny\\source\\repos\\SGU\\SGU\\Users.db");
+    {
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory; 
+        optionsBuilder.UseSqlite($"Data Source={baseDir}Users.db");
+    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
