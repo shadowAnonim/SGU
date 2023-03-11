cd9eac332e9cff54e06f05d298531ee074095e4c
shadowAnonim <shadow.anonim@yandex.ru>
2023-03-11 17:38:26 +0300

    Utils.Error

--- SGU/Utils.cs
+++ SGU/Utils.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SGU
 {
     static internal class Utils
     {
         public static UsersContext db = new UsersContext();
+
+        public static void Error(string text)
+        {
+            MessageBox.Show(text, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
 
         public static bool checkEmail(string input)
         {
