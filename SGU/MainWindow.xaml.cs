38e90480657cb96c1dd25847fc7c080f891d231c
shadowAnonim <shadow.anonim@yandex.ru>
2023-03-11 14:45:59 +0300

    Добавьте файлы проекта.

---
+++ SGU/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SGU
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void frame_Navigated(object sender, NavigationEventArgs e)
+        {
+            Page content = e.Content as Page;
+            titleLbl.Content = content.Title;
+            if (content is MainPage)
+                backBtn.Visibility = Visibility.Collapsed;
+            else
+                backBtn.Visibility = Visibility.Visible;
+        }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            frame.NavigationService.GoBack();
+        }
+    }
+}
